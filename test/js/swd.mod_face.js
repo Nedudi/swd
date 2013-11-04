8b2bd3e1b00982816e65616397905e0be0be3676
Alexey Puzenko <alexey82.work@gmail.com>
2013-11-05 00:40:03 +0300

    face detect back to bbf (test)

--- test/js/swd.mod_face.js
+++ test/js/swd.mod_face.js
@@ -7,18 +7,18 @@
     this.width = 640;
     this.height = 480;
     this._img_u8 = null;
-    this._edg = null;
-    this._ii_sum = null;
-    this._ii_sqsum = null;
-    this._ii_tilted = null;
-    this._ii_canny = null;
+//    this._edg = null;
+//    this._ii_sum = null;
+//    this._ii_sqsum = null;
+//    this._ii_tilted = null;
+//    this._ii_canny = null;
 
     // sizes for canvas to detect face
     this._maxFaceCanvasSize = 160;
     this._detectFaceWidth = 0;
     this._detectFaceHeight = 0;
 //    this._classifier = jsfeat.haar.eye;
-    this._classifier = jsfeat.haar.frontalface;
+//    this._classifier = jsfeat.haar.frontalface;
 
     this._initParameters(layers);
   }
@@ -36,16 +36,15 @@
     this._img_u8 = new jsfeat.matrix_t(this._detectFaceWidth, this._detectFaceHeight, jsfeat.U8_t | jsfeat.C1_t);
     jsfeat.bbf.prepare_cascade(jsfeat.bbf.face_cascade);
 
-    this._edg = new jsfeat.matrix_t(this._detectFaceWidth, this._detectFaceHeight, jsfeat.U8_t | jsfeat.C1_t);
-    this._ii_sum = new Int32Array((this._detectFaceWidth+1)*(this._detectFaceHeight+1));
-    this._ii_sqsum = new Int32Array((this._detectFaceWidth+1)*(this._detectFaceHeight+1));
-    this._ii_tilted = new Int32Array((this._detectFaceWidth+1)*(this._detectFaceHeight+1));
-    this._ii_canny = new Int32Array((this._detectFaceWidth+1)*(this._detectFaceHeight+1));
-    //    jsfeat.bbf.prepare_cascade(jsfeat.haar.eye);
+//    this._edg = new jsfeat.matrix_t(this._detectFaceWidth, this._detectFaceHeight, jsfeat.U8_t | jsfeat.C1_t);
+//    this._ii_sum = new Int32Array((this._detectFaceWidth+1)*(this._detectFaceHeight+1));
+//    this._ii_sqsum = new Int32Array((this._detectFaceWidth+1)*(this._detectFaceHeight+1));
+//    this._ii_tilted = new Int32Array((this._detectFaceWidth+1)*(this._detectFaceHeight+1));
+//    this._ii_canny = new Int32Array((this._detectFaceWidth+1)*(this._detectFaceHeight+1));
+//    jsfeat.bbf.prepare_cascade(jsfeat.haar.eye);
   };
 
   Mod_Face.prototype.process = function(layers) {
-
     var context1 = layers.face1.getContext("2d");
     var context2 = layers.face2.getContext("2d");
     context1.drawImage(layers.camera, 0, 0, this._detectFaceWidth, this._detectFaceHeight);
@@ -53,13 +52,13 @@
 
     window.timeStart();
     window.graph.filters.grayscaleModified(imageData.data, imageData.data);
-    window.graph.filters.brightnesAndContrast(
-      imageData.data,
-      imageData.data,
-      window.swd.params.Brightness,
-      window.swd.params.Contrast,
-      window.swd.params.Intensity
-    );
+//    window.graph.filters.brightnesAndContrast(
+//      imageData.data,
+//      imageData.data,
+//      window.swd.params.Brightness,
+//      window.swd.params.Contrast,
+//      window.swd.params.Intensity
+//    );
     context2.putImageData(imageData, 0, 0);
 
     jsfeat.imgproc.grayscaleRGBAToByte(imageData.data, this._img_u8.data);
@@ -67,32 +66,31 @@
 
     var rects;
     /* bbf */
-//    window.timeStart();
-//    var pyr = jsfeat.bbf.build_pyramid(this._img_u8, 24*2, 24*2, 4);
-//    window.timeEnd("build_pyramid");
-//    window.timeStart();
-//    rects = jsfeat.bbf.detect(pyr, jsfeat.bbf.face_cascade);
-//    window.timeEnd("detect");
-//    window.timeStart();
-//    rects = jsfeat.bbf.group_rectangles(rects, 1);
-//    window.timeEnd("group_rectangles");
-//
-//    var on = rects.length;
-//    if(on) {
-//      jsfeat.math.qsort(rects, 0, on-1, function(a,b){ return (b.confidence<a.confidence); });
-//    }
-//    var n = 1 || on;
+    window.timeStart();
+    var pyr = jsfeat.bbf.build_pyramid(this._img_u8, 24*2, 24*2, 4);
+    window.timeEnd("build_pyramid");
+    window.timeStart();
+    rects = jsfeat.bbf.detect(pyr, jsfeat.bbf.face_cascade);
+    window.timeEnd("detect");
+    window.timeStart();
+    rects = jsfeat.bbf.group_rectangles(rects, 1);
+    window.timeEnd("group_rectangles");
+
+    var on = rects.length;
+    if(on) {
+      jsfeat.math.qsort(rects, 0, on-1, function(a,b){ return (b.confidence<a.confidence); });
+    }
+    var n = 1 || on;
 
     /* haaf */
-    jsfeat.imgproc.compute_integral_image(this._img_u8, this._ii_sum, this._ii_sqsum, this._classifier.tilted ? ii_tilted : null);
+//    jsfeat.imgproc.compute_integral_image(this._img_u8, this._ii_sum, this._ii_sqsum, this._classifier.tilted ? ii_tilted : null);
 //    if(options.use_canny) {
 //      jsfeat.imgproc.canny(img_u8, edg, 10, 50);
 //      jsfeat.imgproc.compute_integral_image(edg, ii_canny, null, null);
 //    }
-    jsfeat.haar.edges_density = swd.params.edges_density;
-    rects = jsfeat.haar.detect_multi_scale(this._ii_sum, this._ii_sqsum, this._ii_tilted, swd.params.use_canny ? this._ii_canny : null, this._img_u8.cols, this._img_u8.rows, this._classifier, swd.params.scale_factor, swd.params.min_scale);
-    rects = jsfeat.haar.group_rectangles(rects, 1);
-
+//    jsfeat.haar.edges_density = swd.params.edges_density;
+//    rects = jsfeat.haar.detect_multi_scale(this._ii_sum, this._ii_sqsum, this._ii_tilted, swd.params.use_canny ? this._ii_canny : null, this._img_u8.cols, this._img_u8.rows, this._classifier, swd.params.scale_factor, swd.params.min_scale);
+//    rects = jsfeat.haar.group_rectangles(rects, 1);
 
     var r = rects[0];
     if(!r) { return null; }
