7dfefd16f8f86962d88de342a683b563cd4d6457
Dmitry Dudin <dudin@xbsoftware.com>
2013-12-12 02:56:53 +0300

    fixes, audioclick and etc

--- ext_iframe/js/swd.js
+++ ext_iframe/js/swd.js
@@ -4,16 +4,7 @@
 
   window.swd = window.swd || {};
 
-
-
-  // window.swd.addEventListener("swdCursorPosition", function(data) {
-  //   //setCursorPosition(data);
-  // });
-
-
-
   window.swd.addEventListener("swdCursorPosition", function(data) {
-    //console.log('====',data)
     window.swd.ask('swdCursorPosition',data);
   });
 
@@ -22,45 +13,19 @@
   });
 
   window.swd.addEventListener("swdAudioClick", function(data) {
+     console.log('!!!!!!!!!!!!AAAAAAAA')
      window.swd.ask('swdAudioClick',data);
   });
 
-  // window.swd.on('swdCursorPosition', function(data){
-  //   console.log('==>swdCursorPosition',data)
-  // });
-
-  // function addJsFileToHead(data) {
-  //   var script = document.createElement("script");
-  //   script.setAttribute("type", "text/javascript");
-  //   script.innerHTML = data;
-  //   document.getElementsByTagName("head")[0].appendChild(script);
-  // }
-
-  // $.get(chrome.extension.getURL('js/main.js'),
-  //   function(data) {
-  //     addJsFileToHead(data);
-  //     document.getElementsByTagName("body")[0].setAttribute("onLoad", "injected_main();");
-  //   }
-  // );
-
-
-
 
   // Lesha hello, options change can be detected like this
-    swd.on('messageSettingsChanged', function(request){
-      console.log('!!!!!!==== messageSettingsChanged ====>>>>',request);
-    });
-
+  swd.on('messageSettingsChanged', function(request){
+    console.log('!!!!!!==== messageSettingsChanged ====>>>>',request);
+  });
 
 
   document.addEventListener('webkitvisibilitychange', function(){
-    // if(document.webkitHidden){
-    //   console.log('disable srv frame')
-    //   window.swd.sendMessage("disable");
-    // } else {
-    //   console.log('enable srv frame')
-    //   window.swd.sendMessage("enable");
-    // }
+    // if(document.webkitHidden){........
   }, false);
 
 
